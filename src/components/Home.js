360ef8abf292ae511ca578ea7aa96b71a48aa091
carterhanks <carterhanks@gmail.com>
2021-11-02 17:32:42 -0600

    Added additional Sections to be rendered, edited props/state

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,7 +5,34 @@ import Section from "./Section";
 function Home() {
 	return (
 		<div className="Container">
-			<Section />
+			<Section
+				title="Model S"
+				description="Order Online for Touchless Delivery"
+				backgroundImg="model-s.jpeg"
+				leftBtnText="Custom Order"
+				rightBtnText="Existing Inventory"
+			/>
+			<Section
+				title="Model Y"
+				description="Order Online for Touchless Delivery"
+				backgroundImg="model-y.jpeg"
+				leftBtnText="Custom Order"
+				rightBtnText="Existing Inventory"
+			/>
+			<Section
+				title="Model 3"
+				description="Order Online for Touchless Delivery"
+				backgroundImg="model-3.jpeg"
+				leftBtnText="Custom Order"
+				rightBtnText="Existing Inventory"
+			/>
+			<Section
+				title="Model X"
+				description="Order Online for Touchless Delivery"
+				backgroundImg="model-x.jpeg"
+				leftBtnText="Custom Order"
+				rightBtnText="Existing Inventory"
+			/>
 		</div>
 	);
 }
--- src/components/Section.js
+++ src/components/Section.js
@@ -2,17 +2,23 @@ import React from "react";
 import "../stylesheets/Section.css";
 import DownArrow from "../images/down-arrow.svg";
 
-function Section() {
+function Section({
+	title,
+	description,
+	leftBtnText,
+	rightBtnText,
+	backgroundImg
+}) {
 	return (
-		<div className="Wrap">
+		<div className="Wrap" bgImage={backgroundImg}>
 			<div className="ItemText">
-				<h1>Model S</h1>
-				<p>Order Online for Touchless Delivery</p>
+				<h1>{title}</h1>
+				<p>{description}</p>
 			</div>
 			<div className="Buttons">
 				<div className="ButtonGroup">
-					<div className="LeftButton">Custom Order</div>
-					<div className="RightButton">Existing Inventory</div>
+					<div className="LeftButton">{leftBtnText}</div>
+					<div className="RightButton">{rightBtnText}</div>
 				</div>
 				<img src={DownArrow} className="DownArrow"></img>
 			</div>
