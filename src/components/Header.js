a75558dc02b8f5a8c2cb14aeed562555e7d5782f
carterhanks <carterhanks@gmail.com>
2021-11-05 13:06:48 -0600

    Added hamburger menu info and MaterialUI Icons

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,6 +2,7 @@ import React from "react";
 import "../stylesheets/Header.css";
 import logo from "../images/logo.svg";
 import MenuIcon from "@mui/icons-material/Menu";
+import CloseIcon from "@mui/icons-material/Close";
 
 function Header() {
 	return (
@@ -22,6 +23,68 @@ function Header() {
 				<a href="#">Shop</a>
 				<a href="#">Tesla Account</a>
 				<MenuIcon className="header-rightMenu-customMenu"></MenuIcon>
+			</div>
+			<div className="header-burgerNav">
+				<div className="header-burgerNav-closeWrapper">
+					<CloseIcon className="header-burgerNav-closeWrapper-closeIcon" />
+				</div>
+				<li>
+					<a href="#">Model S</a>
+				</li>
+				<li>
+					<a href="#">Model 3</a>
+				</li>
+				<li>
+					<a href="#">Model X</a>
+				</li>
+				<li>
+					<a href="#">Model Y</a>
+				</li>
+				<li>
+					<a href="#">Solar Roof</a>
+				</li>
+				<li>
+					<a href="#">Solar Panels</a>
+				</li>
+				<li>
+					<a href="#">Existing Inventory</a>
+				</li>
+				<li>
+					<a href="#">Used Inventory</a>
+				</li>
+				<li>
+					<a href="#">Test Drive</a>
+				</li>
+				<li>
+					<a href="#">Powerwall</a>
+				</li>
+				<li>
+					<a href="#">Commercial Energy</a>
+				</li>
+				<li>
+					<a href="#">Utilities</a>
+				</li>
+				<li>
+					<a href="#">Charging</a>
+				</li>
+				<li>
+					<a href="#">Find Us</a>
+				</li>
+				<li>
+					<a href="#">Support</a>
+				</li>
+				<li>
+					<a href="#">Investor Relations</a>
+				</li>
+				<li>
+					<a href="#">Shop</a>
+				</li>
+				<li>
+					<a href="#">Account</a>
+				</li>
+				<li>
+					<a href="#">More</a>
+				</li>
 			</div>
 		</div>
 	);
--- src/components/Section.js
+++ src/components/Section.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "../stylesheets/Section.css";
 import downArrow from "../images/new-down-arrow.svg";
+import Fade from "react-reveal/Fade";
 
 function Section({
 	title,
@@ -11,15 +12,19 @@ function Section({
 }) {
 	return (
 		<div className="Wrap" style={{ backgroundImage: `url(${backgroundImg})` }}>
-			<div className="ItemText">
-				<h1>{title}</h1>
-				<p>{description}</p>
-			</div>
-			<div className="Buttons">
-				<div className="ButtonGroup">
-					<div className="LeftButton">{leftBtnText}</div>
-					{rightBtnText && <div className="RightButton">{rightBtnText}</div>}
+			<Fade bottom>
+				<div className="ItemText">
+					<h1>{title}</h1>
+					<p>{description}</p>
 				</div>
+			</Fade>
+			<div className="Buttons">
+				<Fade bottom>
+					<div className="ButtonGroup">
+						<div className="LeftButton">{leftBtnText}</div>
+						{rightBtnText && <div className="RightButton">{rightBtnText}</div>}
+					</div>
+				</Fade>
 				<img src={downArrow} className="DownArrow"></img>
 			</div>
 		</div>
